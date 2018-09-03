050b36faef8a94e023f1aad45fdc0edabc1701e8
Oleg Nasteka <oleg.nasteka@gmail.com>
2018-09-03 19:38:07 +0300

    Object Methods Options

--- js/object.js
+++ js/object.js
@@ -22,7 +22,7 @@ const obj1 = {
 
 var result = obj;
 
-var obj2 = new Object(); // var ob2 = {};
+var obj2 = Object.create(obj1); // var ob2 = {};
 
 // document.getElementById('result').innerHTML = result.options + ' ' + result.name;
 
@@ -83,6 +83,11 @@ console.log('-----------------------');
 
 function iteration(o) {
     for (v in o) {
+
+        if(!o.hasOwnProperty(v)) continue;
+
+        if (typeof o[v] === 'function') continue;
+
         if (typeof o[v] == 'object'){
             console.log(v + ':');
             iteration(o[v]);
@@ -94,10 +99,121 @@ function iteration(o) {
     }
 }
 
-iteration(obj1);
+// iteration(obj2);
 
 // for (v in obj1){
 //
 //     console.log(v + ' - ' + obj1[v]);
 //
 // }
+
+// if(obj1.name) {
+//     alert(obj1.name);
+// }
+//
+// if('name' in obj1){
+//     alert(obj1.name + ' 222');
+// }
+
+// if(obj2.hasOwnProperty('name')){
+//     alert(obj2.name);
+// }
+
+// if(obj1.propertyIsEnumerable('name')){
+//     alert(obj1.name);
+// }
+
+/*
+*
+* Method
+*
+ */
+
+
+/*
+var obj3 = {
+    prop1: 50,
+    prop2: 20,
+    func: function () {
+        console.log(this.prop1 + this.prop2);
+
+        foo(this);
+    }
+};
+
+obj3.func2 = function(){
+    console.log('New method!');
+};
+
+function foo(o){
+    console.log(o);
+}
+
+
+obj3.func3 = foo;
+
+obj3.func();
+*/
+
+
+var o = {
+    prop1: 10,
+    prop2: 20,
+    func: function () {
+        console.log(this.prop1 + this.prop2);
+    },
+    get summ() {
+        return this.prop1 + this.prop2;
+    },
+    set summ(value) {
+        this.prop1 += value;
+    }
+};
+
+/*
+console.log(o.summ + ' summ');
+
+console.log(o.prop1 + ' old');
+
+o.setProp = 20;
+
+console.log(o.prop1 + ' new');
+
+console.log(o.summ + ' new summ');
+*/
+
+Object.defineProperty(o, 'prop3', {
+    value: 100,
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty(o, 'prop3', {
+    value: 10,
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+// iteration(o);
+
+
+// var o2 = new foo(a,b,c);
+
+
+function People(name, age) {
+    this.name = name;
+    this.age = age;
+
+    this.summ = function () {
+        return this.name + ' - ' + this.age;
+    }
+}
+
+People.prototype = obj1;
+People.prototype =
+
+var Ben = new People('Ben', 32);
+
+console.log(Ben.summ());
