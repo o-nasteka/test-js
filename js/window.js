95bdf68f46a90ee310d31aa8123a2683610f01c6
Oleg Nasteka <oleg.nasteka@gmail.com>
2018-09-04 15:05:43 +0300

    Modal window with animation
    Close modal with animation

--- js/window.js
+++ js/window.js
@@ -13,10 +13,16 @@
 window.onload = function () {
     var wrap = document.getElementById('popup_overlay');
     var closeB = document.getElementById('popup_close');
+    closeB.onclick = popupClose;
     
     function popup() {
         wrap.style.display = 'block';
         popupIn(1);
+    }
+    
+    function popupClose() {
+        // wrap.style.display = 'none';
+        popupOut(0);
     }
 
     // opacity 0.05 0.1 0.15 ....
@@ -32,6 +38,19 @@ window.onload = function () {
         }
     }
 
-    setTimeout(popup, 3000);
+    // opacity 0.05 0.1 0.15 ....
+    function popupOut(x){
+        var op = (wrap.style.opacity) ? parseFloat(wrap.style.opacity) : 1 ;
+
+        if(op > x){
+            op -= 0.05;
+            wrap.style.opacity = op;
+            setTimeout(function () {
+                popupOut(x);
+            },50)
+        }
+    }
+
+    setTimeout(popup, 1000);
 };
 
