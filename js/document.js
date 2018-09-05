03d20298fee04a8927804b12e80836ed8583d301
Oleg Nasteka <oleg.nasteka@gmail.com>
2018-09-05 15:02:38 +0300

    get by id get all images, links, form elements add style

--- js/document.js
+++ js/document.js
@@ -22,6 +22,33 @@ window.onload = function () {
 
     }
 
-    var result = getElements('stuck_container', 'content', 'footer');
-    console.log(result);
+    // var result = getElements('stuck_container', 'content', 'footer');
+    // console.log(result);
+
+    // var elname = document.getElementsByName('email');
+    // var elname2 = document.getElementsByName('email')[0].style.border = '3px solid red';
+    // console.log(elname);
+
+    // var divMy = document.getElementById('divId'); // Element Id
+
+    // var eltag = document.getElementsByTagName('p');  // Tag Name
+
+
+    // console.log(document.images);  // All Images
+
+    // console.log(document.links); // All links
+
+    // console.log(document.forms); // Forms elements
+
+
+    // Get All Images and add 3px Red Border
+    /*
+    for (var i = 0; i < document.images.length; i++) {
+        document.images[i].style.border = '3px solid red';
+    }
+    */
+
+    // Get element by class
+
+
 }
