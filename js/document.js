71bb5aa6f1eab9ab801834448e09b8c749ac3c89
Oleg Nasteka <oleg.nasteka@gmail.com>
2018-09-05 14:40:09 +0300

    func getElbyId => put it in Object

--- js/document.js
+++ js/document.js
@@ -1,1 +1,27 @@
-alert('document js')
+window.onload = function () {
+    var elId = document.getElementById('stuck_container');
+
+    function getElements() {
+        var elem = {};
+
+        //Arguments
+        for (var i = 0; i < arguments.length; i++){
+            var id = arguments[i];
+
+            var el = document.getElementById(id);
+
+            if (el == null) {
+                continue;
+            }
+
+            elem[id] = el;
+        }
+
+        return elem;
+
+
+    }
+
+    var result = getElements('stuck_container', 'content', 'footer');
+    console.log(result);
+}
